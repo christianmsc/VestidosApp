cfc009711455550cf9a45d53339835ad5d1b7b88
christianmsc <christian1601y@hotmail.com>
2019-11-20 13:56:09 -0300

    Ajustes na tela de detalhes de vestidos

--- src/app/vestido-detalhes/vestido-detalhes.page.ts
+++ src/app/vestido-detalhes/vestido-detalhes.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { VestidoService } from '../services/vestido.service';
-import { ModalController } from '@ionic/angular';
+import { ModalController, LoadingController } from '@ionic/angular';
 import { ImgFullModalPage } from '../img-full-modal/img-full-modal.page';
 
 @Component({
@@ -13,10 +13,15 @@ export class VestidoDetalhesPage implements OnInit {
 
   public id: number;
   public vestido: any;
+  public loading: any;
 
-  constructor(private router: ActivatedRoute, private vestidoService: VestidoService, public modalController: ModalController) { }
+  constructor(private router: ActivatedRoute, 
+              private vestidoService: VestidoService, 
+              public modalController: ModalController,
+              public loadingController: LoadingController) { }
 
   ngOnInit() {
+    this.showLoading();
     this.id = Number(this.router.snapshot.paramMap.get('id'));
     this.vestidoService.getVestidos(this.id).subscribe((data: any) => {
 
@@ -24,11 +29,12 @@ export class VestidoDetalhesPage implements OnInit {
 
       if (data.sucesso) {
         this.vestido = data.vestido;
-        console.log(this.vestido);
-        
       }
 
+      this.dismissLoading();
+
     }, err => {
+      this.dismissLoading();
       alert(err);
     });
   }
@@ -42,4 +48,18 @@ export class VestidoDetalhesPage implements OnInit {
     });
     return await modal.present();
   }
+
+  async showLoading() {
+    this.loading = await this.loadingController.create({
+      message: 'ta saindo do armário o vestido =D',
+      translucent: true,
+      cssClass: 'loading'
+    });
+    return await this.loading.present();
+  }
+
+  async dismissLoading(){
+    return await this.loading.dismiss();
+  }
+
 }
